8453f2e212fb3859d2be8e5745c33d7bc4d94fc9
Ha Mi <63441133+gerichilli@users.noreply.github.com>
2021-03-26 23:07:17 +0700

    search bar

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,7 @@
 import "../sass/main.scss";
 import * as model from './model.js';
 import recipeView from './view/recipeView.js';
+import searchView from './view/searchView.js';
 
 
 
@@ -26,8 +27,26 @@ const controlRecipe = async function() {
   }
 }
 
+const controlSearchResults = async function() {
+  try {
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if(!query) return;
+
+    // 2) Load search results
+    model.loadSearchResults(query);
+    
+    // 3) Render results
+    console.log(model.state.search.results);
+
+  } catch(err) {
+    console.log(err);
+  }
+}
+
 const init = function() {
   recipeView.addHandlerRender(controlRecipe);
+  searchView.addHandlerSearch(controlSearchResults);
 }
 
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -3,6 +3,10 @@ import { getJSON } from './helpers.js';
 
 export const state = {
     recipe: {},
+    search: {
+      query:'',
+      results: []
+    }
 };
 
 export const loadRecipe = async function(id) {
@@ -24,3 +28,25 @@ export const loadRecipe = async function(id) {
     throw err; 
   }
 }
+
+export const loadSearchResults = async function(query) {
+  try {
+    state.search.query = query;
+
+    const data = await getJSON(`${API_URL}?search=${query}`);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+
+      }
+    });
+
+  } catch(err) {
+    console.log(err);
+    throw(err)
+  }
+};
